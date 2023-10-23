a1bb3c3867f308ca68304ff106380d8661483a3b
santiagogimellig <santi_gimelli@hotmail.com.ar>
2023-10-23 10:57:29 -0300

    arreglo un error en app

--- desafioEntregable4/src/app.js
+++ desafioEntregable4/src/app.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import exphbs from 'express-handlebars';
+import handlebars from 'express-handlebars';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 8080;
 
-app.engine('.hbs', exphbs({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
