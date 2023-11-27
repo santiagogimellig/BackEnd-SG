df00ddc65c29df592f1799ea1e674d7ca54cabba
Santiago Gimelli <108678224+santiagogimellig@users.noreply.github.com>
2023-11-27 16:05:57 -0300

    Delete segundaPreEntrega directory

--- segundaPreEntrega/src/server.js
+++
@@ -1,9 +0,0 @@
-import http from 'http';
-import app from './app.js';
-
-const server = http.createServer(app);
-const PORT = 8080;
-
-server.listen(PORT, () => {
-    console.log(`Server running in http://localhost:${PORT} 🚀`);
-});
