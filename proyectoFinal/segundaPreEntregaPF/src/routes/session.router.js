c96d460749b680ba499b4f32d1f2d6bb57b0a18c
santiagogimellig <santi_gimelli@hotmail.com.ar>
2023-12-18 19:07:25 -0300

    arreglo ruta

--- proyectoFinal/segundaPreEntregaPF/src/routes/session.router.js
+++ proyectoFinal/segundaPreEntregaPF/src/routes/session.router.js
@@ -5,12 +5,25 @@ import passport from "passport";
 
 const router = Router();
 
+router.get('/sessions/logout', (req, res) => {
+    req.session.destroy((error) => {
+        res.redirect('/');
+    });
+});
+
+router.get('/sessions/github', passport.authenticate('github', { scope: ['user:email'] }))
+router.get('/sessions/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/products');
+})
+
 router.post('/sessions/register', async (req, res) => {
     const { body } = req
     const newUser = await userModel.create({ ...body, password: createHash(body.password) })
     console.log('newUser', newUser);
     res.redirect('/login');
 })
+
 
 router.post('/sessions/register', passport.authenticate('register', { failureRedirect: '/register' }), (req, res) => {
     res.redirect('/login');
@@ -39,15 +52,11 @@ router.post('/sessions/login', async (req, res) => {
     req.session.user = { first_name, last_name, email };
     res.redirect(`/products`);
 });
+
 router.post('/sessions/login', passport.authenticate('login', { failureRedirect: '/' }), (req, res) => {
     req.session.user = req.user;
     res.redirect('/products');
 });
-router.get('/sessions/github', passport.authenticate('github', { scope: ['user:email'] }))
-router.get('/sessions/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
-    req.session.user = req.user;
-    res.redirect('/products');
-})
 
 router.post('/sessions/recovery-password', async (req, res) => {
     const { email, newPassword } = req.body;
@@ -57,12 +66,6 @@ router.post('/sessions/recovery-password', async (req, res) => {
     }
     await userModel.updateOne({ email }, { $set: { password: createHash(newPassword) } });
     res.redirect('/login');
-});
-
-router.get('/sessions/logout', (req, res) => {
-    req.session.destroy((error) => {
-        res.redirect('/');
-    });
 });
 
 export default router;
